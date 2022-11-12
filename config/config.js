c8a90c9cede539e9d4757bb01a19f1bcd37cb195
Ali Lefta <36763624+alilefta@users.noreply.github.com>
2022-11-13 00:16:23 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,6 +5,6 @@ const config = {
 	mongoUri:
 		process.env.MONGODB_URI ||
 		process.env.MONGO_HOST ||
-		"mongodb+srv://mern_simplelearn:mern@121122@cluster0.jx6wun4.mongodb.net/?retryWrites=true&w=majority",
+		"",
 };
 export default config;
